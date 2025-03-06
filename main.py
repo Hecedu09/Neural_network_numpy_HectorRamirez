c7cd88eb3adb3d208a16d51cc8527a3e5b8bbddf
Hecedu09 <hec.ed.rmg@gmail.com>
2025-03-06 00:15:48 -0600

    Actualizacon 1

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.numpycode import train_model
+
+# Punto de entrada del script
+if __name__ == "__main__":
+    """
+    Ejecuta la función principal de entrenamiento del modelo cuando el script se ejecuta directamente.
+    """
+    train_model()
